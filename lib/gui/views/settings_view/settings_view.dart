125244741574bbe2071d46bcebf01ba059de80e6
Stiven Gomez <stiven.gomez@nuvu.cc>
2022-07-26 22:44:38 -0500

    Settings view.

---
+++ lib/gui/views/settings_view/settings_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_series_app/gui/app_style.dart';
+
+class SettingsView extends StatelessWidget {
+  const SettingsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppStyle.darkBackground,
+      appBar: AppBar(
+        title: Text("Configuración", style: Theme.of(context).textTheme.headline6!.copyWith(color: AppStyle.whiteColor),),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+    );
+  }
+}
