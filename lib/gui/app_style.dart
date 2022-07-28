b386a7527c0ea64fbda597c3accdfef34a91d5d4
Stiven Gomez <stiven.gomez@nuvu.cc>
2022-07-27 21:51:38 -0500

    Color navigationBar.

--- lib/gui/app_style.dart
+++ lib/gui/app_style.dart
@@ -6,6 +6,7 @@ class AppStyle {
   static const Color whiteColor = Colors.white;
   static const Color darkBackground = Color(0xFF191919);
   static const Color blackColor = Colors.black;
+  static const Color colorNavigationBar = Color(0xFF0F0F0F);
   static const Color greyColor = Color(0xFF8C8C8C);
   static const Color transparent = Colors.transparent;
 
